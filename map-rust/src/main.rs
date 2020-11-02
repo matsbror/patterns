92a9a90895be9c44acc4c8ffd2a38022aedde30b
Mats Brorsson <mats@embeinnovation.com>
2020-11-02 20:58:13 +0100

    added rust code

--- map-rust/src/main.rs
+++ map-rust/src/main.rs
@@ -1,28 +1,41 @@
 extern crate rand;
 
-use std::time::{Instant};
 use rand::thread_rng;
 use rand::Rng;
+use std::time::{Duration, Instant};
+
+const N: u32 = 10;
 
 fn main() {
     let mut rng = thread_rng();
-    
-    let arr: Vec<i64> = (0..200000000).map(|_| rng.gen_range(-100, 100)).collect();
+    let mut seq1_duration = Duration::new(0,0);
+    let mut seq2_duration = Duration::new(0,0);
 
-    let start = Instant::now();
-    let arr_result = map_seq(&arr, |i| i*2);
-    let duration = start.elapsed();
-    
-    println!("Time for serial map: {:?}", duration);
+    for _ in 0..N {
+        let mut arr: Vec<i64> = (0..200000000).map(|_| rng.gen_range(-100, 100)).collect();
+        let arr_copy = arr.clone();
 
-    for i in 0..200000000 {
-        assert!(arr[i] *2 == arr_result[i]);
+        let start = Instant::now();
+        map_seq(arr, |i| i * 2);
+        seq1_duration += start.elapsed();
+
+        arr = arr_copy.clone();
+        let start = Instant::now();
+        map_seq2(arr, |i| i * 2);
+        seq2_duration += start.elapsed();
     }
-    
-
+    println!("Time for seq1 map: {:?}", seq1_duration/N);
+    println!("Time for seq2 map: {:?}", seq2_duration/N);
 }
 
-
-fn map_seq(arr: & Vec<i64>, foo: fn(&i64) -> i64) -> Vec<i64> {
+fn map_seq(arr: Vec<i64>, foo: fn(&i64) -> i64) -> Vec<i64> {
     arr.iter().map(foo).collect()
+}
+
+fn map_seq2(mut arr: Vec<i64>, foo: fn(&i64) -> i64) -> Vec<i64> {
+    for i in 0..arr.len() {
+        arr[i] = foo(&arr[i]);
+    }
+
+    arr
 }
