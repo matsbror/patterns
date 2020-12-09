be3678130b28c297a6de6aeac91c43d3b2ddf86e
Mats Brorsson <mats@embeinnovation.com>
2020-12-09 14:42:07 +0100

    tidied up code

--- map_reduce/map-reduce.cpp
+++ map_reduce/map-reduce.cpp
@@ -22,21 +22,18 @@ int mapred_stl(std::vector<int> &arr1, std::vector<int> &arr2) {
         [](int a, int b){return a+b;},      // reduction operator
         [](int a, int b){return a*b;}      // map operator
         );                           
-    
 }
 
 int mapred_parstl(std::vector<int> &arr1, std::vector<int> &arr2) {
     return transform_reduce(
-        std::execution::par_unseq,
+        execution::par_unseq,
         arr1.begin(), arr1.end(), arr2.begin(), 
         0,
         [](int a, int b){return a+b;},      // reduction operator
         [](int a, int b){return a*b;});     // map operator
-
 }
 
 int mapred_for(std::vector<int> &arr1, std::vector<int> &arr2){
-
     int sum = 0;
     for (long i = 0; i < arr1.size(); ++i) {
         sum = sum + arr1[i] * arr2[i];
@@ -45,7 +42,6 @@ int mapred_for(std::vector<int> &arr1, std::vector<int> &arr2){
 }
 
 int mapred_openmp(std::vector<int> &arr1, std::vector<int> &arr2){
-
     int sum = 0;
     #pragma omp parallel for reduction(+:sum)
     for (long i = 0; i < arr1.size(); ++i) {
@@ -56,9 +52,7 @@ int mapred_openmp(std::vector<int> &arr1, std::vector<int> &arr2){
 
 int mapred_threads(std::vector<int> &arr1, std::vector<int> &arr2){
     const auto num_procs = std::thread::hardware_concurrency();
-
     vector<thread> tpool(num_procs); // create a thread pool
-
     vector<int> results(num_procs);
 
     for (int p = 0; p < num_procs; p++) {
@@ -75,21 +69,16 @@ int mapred_threads(std::vector<int> &arr1, std::vector<int> &arr2){
             results[p] = my_sum;
         });
     }
-
-
     // wait for threads to finish
     int sum = 0;
     for (int p = 0; p < num_procs; p++) {
         tpool[p].join();
         sum = sum + results[p];
     }
-
     return sum;
-
 }
 
 int mapred_tbb(vector<int> &arr1,vector<int> &arr2) {
-
     return tbb::parallel_reduce(
         tbb::blocked_range<int>(0, arr1.size()),
         0,
@@ -101,7 +90,6 @@ int mapred_tbb(vector<int> &arr1,vector<int> &arr2) {
         },
         [](int a, int b) {return a+b;}
         );
-
 }
 
 double
